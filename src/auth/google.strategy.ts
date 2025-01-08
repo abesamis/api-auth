6e55df71964302d4e51a3c8b59b79a23b22e3695
Wilson Abesamis <wlabesamis@gmail.com>
2025-01-08 14:01:51 +0800

    added debug code

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -5,14 +5,19 @@ import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor() {
-    console.log('GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
-    console.log('GOOGLE_CLIENT_SECRET:', process.env.GOOGLE_CLIENT_SECRET);
-    console.log('GOOGLE_CALLBACK:', process.env.GOOGLE_CALLBACK);
+    const googleClientId = process.env.GOOGLE_CLIENT_ID;
+    const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
+    const jwtSecret = process.env.JWT_SECRET;
+    const googleCallback = process.env.GOOGLE_CALLBACK ?? 'http://localhost:3000/auth/google/redirect';
+
+    console.log('GOOGLE_CLIENT_ID:', googleClientId);
+    console.log('GOOGLE_CLIENT_SECRET:', googleClientSecret);
+    console.log('GOOGLE_CALLBACK:', jwtSecret);
     
     super({
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK ?? 'http://localhost:3000/auth/google/redirect',
+      clientID: googleClientId,
+      clientSecret: googleClientSecret,
+      callbackURL: googleCallback,
       scope: ['email', 'profile'],
     });
   }
