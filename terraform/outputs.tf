caadb8d844a34f38a1e0719ee896af36e59715e3
Wilson Abesamis <wlabesamis@gmail.com>
2025-02-20 10:46:08 +0800

    updated cloudbuild to add public access

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "postgres_private_ip" {
   value = google_sql_database_instance.postgres_instance.private_ip_address
 }
+
+output "api_auth_static_ip" {
+  value = google_compute_global_address.api_auth_ip.address
+}
